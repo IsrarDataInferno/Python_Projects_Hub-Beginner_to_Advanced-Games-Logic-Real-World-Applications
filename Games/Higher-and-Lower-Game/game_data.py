153cc192987f8c87e04eab45a303b8b2ad60a1dc
M Israr Ali <157912149+IsrarDataInferno@users.noreply.github.com>
2026-02-12 09:27:02 +0500

    Create game_data.py with celebrity database

---
+++ Games/Higher-and-Lower-Game/game_data.py
@@ -0,0 +1,30 @@
+# Celebrity Database
+
+"""
+A sample database for celebrities with fields:
+- name
+- follower_count
+- description
+- country
+"""
+
+celebrities = [
+    {
+        "name": "Celebrity One",
+        "follower_count": 1000000,
+        "description": "Description of Celebrity One.",
+        "country": "Country One"
+    },
+    {
+        "name": "Celebrity Two",
+        "follower_count": 2000000,
+        "description": "Description of Celebrity Two.",
+        "country": "Country Two"
+    },
+    {
+        "name": "Celebrity Three",
+        "follower_count": 3000000,
+        "description": "Description of Celebrity Three.",
+        "country": "Country Three"
+    }
+]
